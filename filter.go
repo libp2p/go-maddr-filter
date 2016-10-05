d9f56211111ad200d4a1fb404c784870a89a0706
Jeromy <why@ipfs.io>
2016-10-04 18:49:13 -0700

    gx publish 1.1.0

--- filter.go
+++ filter.go
@@ -4,8 +4,8 @@ import (
 	"net"
 	"sync"
 
-	ma "github.com/jbenet/go-multiaddr"
-	manet "github.com/jbenet/go-multiaddr-net"
+	ma "github.com/multiformats/go-multiaddr"
+	manet "github.com/multiformats/go-multiaddr-net"
 )
 
 type Filters struct {
