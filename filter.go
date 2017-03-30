c654a4da7dd049b8d1b45a5be44902768dcdb2b8
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-03-29 23:31:56 -0700

    Implement support for whitelists, default-deny/allow

--- filter.go
+++ filter.go
@@ -8,60 +8,150 @@ import (
 	manet "github.com/multiformats/go-multiaddr-net"
 )
 
-type Filters struct {
-	mu      sync.RWMutex
-	filters map[string]*net.IPNet
+type filterEntry struct {
+	f      *net.IPNet
+	reject bool
 }
 
+// Filters is a structure representing a collection of allow/deny
+// net.IPNet filters, together with the RejectByDefault flag, which
+// represents the default filter policy.
+//
+// Note that the last policy added to the Filters is authoritative.
+type Filters struct {
+	mu              sync.RWMutex
+	RejectByDefault bool
+	filters         []*filterEntry
+}
+
+// NewFilters constructs and returns a new set of net.IPNet filters.
+// By default, the new filter rejects no addresses.
 func NewFilters() *Filters {
 	return &Filters{
-		filters: make(map[string]*net.IPNet),
+		RejectByDefault: false,
+		filters:         make([]*filterEntry, 0),
 	}
 }
 
+func (f *Filters) find(ff *net.IPNet) int {
+	ffs := ff.String()
+	for idx, ft := range f.filters {
+		if ft.f.String() == ffs {
+			return idx
+		}
+	}
+
+	return -1
+}
+
+// AddDialFilter adds a reject rule to the given Filters.  Hosts
+// matching the given net.IPNet filter will be rejected, unless
+// another rule is added which states that they should be accepted.
+//
+// No effort is made to prevent duplication of filters, or to simplify
+// the filters list.
 func (fs *Filters) AddDialFilter(f *net.IPNet) {
 	fs.mu.Lock()
 	defer fs.mu.Unlock()
-	fs.filters[f.String()] = f
+
+	idx := fs.find(f)
+	if idx != -1 {
+		fs.filters[idx].reject = true
+	} else {
+		fs.filters = append(fs.filters, &filterEntry{f: f, reject: true})
+	}
 }
 
+// AddAllowFilter adds an accept rule to the given Filters. Hosts
+// matching the given net.IPNet filter will be accepted, unless
+// another policy is added which states that they should be rejected.
+//
+// No effort is made to prevent duplication of filters, or to simplify
+// the filters list.
+func (fs *Filters) AddAllowFilter(f *net.IPNet) {
+	fs.mu.Lock()
+	defer fs.mu.Unlock()
+
+	idx := fs.find(f)
+	if idx != -1 {
+		fs.filters[idx].reject = false
+	} else {
+		fs.filters = append(fs.filters, &filterEntry{f: f, reject: false})
+	}
+}
+
+// Remove removes all net.IPNet's accept/reject rule(s) from the
+// Filters, if there are matching rules.
+//
+// Makes no distinction between whether the rule is an allow or a
+// deny.
+func (f *Filters) Remove(ff *net.IPNet) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	idx := f.find(ff)
+	if idx != -1 {
+		f.filters = append(f.filters[:idx], f.filters[idx+1:]...)
+	}
+}
+
+// AddrBlocked parses a ma.Multiaddr and, if it can get a valid netip
+// back, applies the Filters returning true if the given address
+// should be rejected, and false if the given address is allowed.
+//
+// If a parsing error occurs, or no filter matches, the Filters
+// default is returned.
 func (f *Filters) AddrBlocked(a ma.Multiaddr) bool {
 	maddr := ma.Split(a)
 	if len(maddr) == 0 {
-		return false
+		return f.RejectByDefault
 	}
 	netaddr, err := manet.ToNetAddr(maddr[0])
 	if err != nil {
-		// if we can't parse it, it's probably not blocked
-		return false
+		// if we cant parse it, its probably not blocked
+		return f.RejectByDefault
 	}
 	netip := net.ParseIP(netaddr.String())
 	if netip == nil {
-		return false
+		return f.RejectByDefault
 	}
 
 	f.mu.RLock()
 	defer f.mu.RUnlock()
+
+	var reject bool = f.RejectByDefault
+
 	for _, ft := range f.filters {
-		if ft.Contains(netip) {
-			return true
+		if ft.f.Contains(netip) {
+			reject = ft.reject
 		}
 	}
-	return false
+
+	return reject
 }
 
+// Filters returns the list of DENY net.IPNet masks
 func (f *Filters) Filters() []*net.IPNet {
 	var out []*net.IPNet
 	f.mu.RLock()
 	defer f.mu.RUnlock()
 	for _, ff := range f.filters {
-		out = append(out, ff)
+		if ff.reject {
+			out = append(out, ff.f)
+		}
 	}
 	return out
 }
 
-func (f *Filters) Remove(ff *net.IPNet) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	delete(f.filters, ff.String())
+// AllowFilters returns the list of ALLOW net.IPNet masks
+func (f *Filters) AllowFilters() []*net.IPNet {
+	var out []*net.IPNet
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	for _, ff := range f.filters {
+		if !ff.reject {
+			out = append(out, ff.f)
+		}
+	}
+	return out
 }
--- filter_test.go
+++ filter_test.go
@@ -7,7 +7,7 @@ import (
 	ma "github.com/multiformats/go-multiaddr"
 )
 
-func TestFilter(t *testing.T) {
+func TestFilterBlocking(t *testing.T) {
 	f := NewFilters()
 
 	_, ipnet, _ := net.ParseCIDR("0.1.2.3/24")
@@ -28,6 +28,7 @@ func TestFilter(t *testing.T) {
 		f.AddDialFilter(ipnet)
 	}
 
+	// These addresses should all be blocked
 	for _, blocked := range []string{
 		"/ip4/1.2.3.4/tcp/123",
 		"/ip4/4.3.2.1/udp/123",
@@ -43,19 +44,119 @@ func TestFilter(t *testing.T) {
 		}
 	}
 
-	for _, notBlocked := range []string{
+	// test that other net intervals are not blocked
+	for _, addr := range []string{
 		"/ip4/1.2.4.1/tcp/123",
 		"/ip4/4.3.2.2/udp/123",
 		"/ip6/fe00::1/tcp/321",
 		"/ip6/fc00::2/udp/321",
 		"",
 	} {
-		maddr, err := ma.NewMultiaddr(notBlocked)
+		maddr, err := ma.NewMultiaddr(addr)
 		if err != nil {
 			t.Error(err)
 		}
 		if f.AddrBlocked(maddr) {
-			t.Fatalf("expected %s to not be blocked", notBlocked)
+			t.Fatalf("expected %s to not be blocked", addr)
+		}
+	}
+}
+
+func TestFilterWhitelisting(t *testing.T) {
+	f := NewFilters()
+
+	// Add default reject filter
+	f.RejectByDefault = true
+
+	// Add a whitelist
+	_, ipnet, _ := net.ParseCIDR("1.2.3.0/24")
+	f.AddAllowFilter(ipnet)
+
+	// That /24 should now be allowed
+	for _, addr := range []string{
+		"/ip4/1.2.3.1/tcp/123",
+		"/ip4/1.2.3.254/tcp/123",
+		"/ip4/1.2.3.254/udp/321",
+	} {
+		maddr, err := ma.NewMultiaddr(addr)
+		if err != nil {
+			t.Error(err)
+		}
+		if f.AddrBlocked(maddr) {
+			t.Fatalf("expected %s to be whitelisted", addr)
+		}
+	}
+
+	// No policy matches these maddrs, they should be blocked by default
+	for _, blocked := range []string{
+		"/ip4/1.2.4.4/tcp/123",
+		"/ip4/4.3.2.1/udp/123",
+		"/ip6/fd00::2/tcp/321",
+		"/ip6/fc00::1/udp/321",
+	} {
+		maddr, err := ma.NewMultiaddr(blocked)
+		if err != nil {
+			t.Error(err)
+		}
+		if !f.AddrBlocked(maddr) {
+			t.Fatalf("expected %s to be blocked", blocked)
+		}
+	}
+}
+
+func TestFiltersRemoveRules(t *testing.T) {
+	f := NewFilters()
+
+	ips := []string{
+		"/ip4/1.2.3.1/tcp/123",
+		"/ip4/1.2.3.254/tcp/123",
+	}
+
+	// Add default reject filter
+	f.RejectByDefault = true
+
+	// Add whitelisting
+	_, ipnet, _ := net.ParseCIDR("1.2.3.0/24")
+	f.AddAllowFilter(ipnet)
+
+	// these are all whitelisted, should be OK
+	for _, addr := range ips {
+		maddr, err := ma.NewMultiaddr(addr)
+		if err != nil {
+			t.Error(err)
+		}
+		if f.AddrBlocked(maddr) {
+			t.Fatalf("expected %s to be whitelisted", addr)
+		}
+	}
+
+	// Test removing the filter. It'll remove multiple, so make a dupe &
+	// a complement
+	f.AddDialFilter(ipnet)
+
+	// Show that they all apply, these are now blacklisted & should fail
+	for _, addr := range ips {
+		maddr, err := ma.NewMultiaddr(addr)
+		if err != nil {
+			t.Error(err)
+		}
+		if !f.AddrBlocked(maddr) {
+			t.Fatalf("expected %s to be blacklisted", addr)
+		}
+	}
+
+	// remove those rules
+	f.Remove(ipnet)
+
+	// our default is reject, so the 1.2.3.0/24 should be rejected now,
+	// along with everything else
+	for _, addr := range ips {
+		maddr, err := ma.NewMultiaddr(addr)
+		if err != nil {
+			t.Error(err)
+		}
+		if !f.AddrBlocked(maddr) {
+			t.Fatalf("expected %s to be blocked", addr)
 		}
 	}
 }
